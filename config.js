8080b3f4308e5c17efb11917593f48e18a378a83
Infinium-8 <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-01-29 15:52:41 +0100

    init

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = "https://api.pool.infinium.space";
+
+var email = "";
+var telegram = "";
+var discord = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.infinium.space/block/{id}";
+var transactionExplorer = "https://explorer.infinium.space/transaction/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
